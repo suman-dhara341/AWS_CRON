76220a66ed7c0ca375015e05ac230bcf3bcf16b9
Suman <sumandhara341@gmail.com>
2025-08-05 16:19:35 +0530

    Error message added

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,12 +1,21 @@
 import { test, expect } from "@playwright/test";
 
 test("login should show welcome message", async ({ page }) => {
-  await page.goto("https://main.dosl6rwrhoxpw.amplifyapp.com/");
+  try {
+    await page.goto("https://main.dosl6rwrhoxpw.amplifyapp.com/");
 
-  await page.getByPlaceholder("Enter email").fill("user@example.com");
-  await page.getByRole("button", { name: "Login" }).click();
-  console.log("Build successfully playwright test");
-  console.log("Build successfully playwright test 2");
-
-  await expect(page.getByText("Welcome, user@example.com!")).toBeVisible();
+    await page.getByPlaceholder("Enter email").fill("user@example.com");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.getByText("Welcome, user@example.com!")).toBeVisible();
+    await expect(page.getByText("sdsds")).toBeVisible();
+    await page.goto("localhost:3000/");
+  } catch (error) {
+    console.error(
+      JSON.stringify({
+        level: "error",
+        timestamp: new Date().toISOString(),
+        message: "An error occurred during the login test",
+      })
+    );
+  }
 });
